cf41db99ca4a4a4fa86f63cace57814e154bda86
Hijena <markoromsak@gmail.com>
2018-03-15 23:36:47 +0100

    First github upload

--- src/Plugin/Block/ChallengeBlock.php
+++ src/Plugin/Block/ChallengeBlock.php
@@ -22,18 +22,26 @@ class ChallengeBlock extends BlockBase {
 
 	public function build() {
 		$node = \Drupal::routeMatch()->getParameter('node');
+		$day = 86400;		// number of seconds in a day
+		// gets value from date field and converts it into  unix timestamp
 		$date = strtotime($node->get('field_event_date')->value);
-		$now = time();
-		$day = 86400;
-		$temp = $now % $day;
-		$now = $now - $temp;
+		// removes time of day from event time, so only days are left
 		$temp = $date % $day;
 		$date = $date - $temp;
 
+		// gets current time
+		$now = time();
+		// removes time of day from current time, so only days are left
+		$temp = $now % $day;
+		$now = $now - $temp;
+		
+		// check if event is in the past
 		if ($date < $now) {
 			$out = 'The event has ended.';
 		}
+		// checks if event is in the future
 		elseif ($date > $now) {
+			// calculates days between current date and event date
 			$diff = $date - $now;
 			$days = round($diff / $day);
 			$out = 'Days left to event start: '.$days;
@@ -42,7 +50,9 @@ class ChallengeBlock extends BlockBase {
 		  $out = 'The event is in progress.';
 		}
 
+		// returns correct output
 		$build = [];
+		$build['#cache']['max-age'] = 0;
 		$build['sample_block_extra_text']['#markup'] = $out;
 		return $build;
 	}
